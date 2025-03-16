8433d1c77dbcf7b8ace796f457e29c857f79b5dd
Walby12 <alby09.bonf@gmail.com>
2025-03-16 13:45:22 +0000

    Added small things ready to put the pipes later

--- main.py
+++ main.py
@@ -1,35 +1,38 @@
-from pyray import *
+import pyray as pr
 from player import Player
+import raylib as rp
 
-init_window(960, 700, "Medieval Flappy Bird")
-set_target_fps(60)
+pr.init_window(960, 700, "Medieval Flappy Bird")
+pr.set_target_fps(60)
 
 player = Player()
 
-while not window_should_close():
+pr.clear_window_state(rp.FLAG_FULLSCREEN_MODE)
+
+while not pr.window_should_close():
     player.move()
 
     player.frame_timer += 1
     if player.frame_timer >= player.frame_speed:
         player.frame_timer = 0
-        player.current_frame = (player.current_frame + 1) % player.frame_count  # Cycle through animation frames
+        player.current_frame = (player.current_frame + 1) % player.frame_count
 
     char_sprite = player.frames[player.current_frame]
 
-    begin_drawing()
-    clear_background(WHITE)
+    pr.begin_drawing()
+    pr.clear_background(pr.WHITE)
 
-    draw_texture_pro(
+    pr.draw_texture_pro(
         char_sprite,
-        Rectangle(0, 0, char_sprite.width, char_sprite.height),
-        Rectangle(int(player.player_pos[0]), int(player.player_pos[1]), char_sprite.width * 5, char_sprite.height * 5),
-        Vector2(0, 0),
+        pr.Rectangle(0, 0, char_sprite.width, char_sprite.height),
+        pr.Rectangle(int(player.player_pos[0]), int(player.player_pos[1]), char_sprite.width * 5, char_sprite.height * 5),
+        pr.Vector2(0, 0),
         0,
-        WHITE
+        pr.WHITE
     )
 
-    end_drawing()
+    pr.end_drawing()
 
 for sprite in player.frames:
-    unload_texture(sprite)
-close_window()
+    pr.unload_texture(sprite)
+pr.close_window()
--- player.py
+++ player.py
@@ -1,34 +1,50 @@
-from pyray import *
-from raylib import *
+import pyray as pr
+import raylib as rp
+import os
 
 class Player:
     def __init__(self):
-        # Load frames for animation
-        self.frame_paths = [
-            r"assets\2D Pixel Dungeon Asset Pack\Character_animation\priests_idle\priest1\v1\priest1_v1_1.png",
-            r"assets\2D Pixel Dungeon Asset Pack\Character_animation\priests_idle\priest1\v1\priest1_v1_2.png",
-            r"assets\2D Pixel Dungeon Asset Pack\Character_animation\priests_idle\priest1\v1\priest1_v1_3.png",
-            r"assets\2D Pixel Dungeon Asset Pack\Character_animation\priests_idle\priest1\v1\priest1_v1_4.png",
-        ]
-        self.frames = [load_texture(path) for path in self.frame_paths]
+        if os.name == "nt":
+            self.frame_paths = [
+                r'assets\2D Pixel Dungeon Asset Pack\Character_animation\priests_idle\priest1\v1\priest1_v1_1.png',
+                r"assets\2D Pixel Dungeon Asset Pack\Character_animation\priests_idle\priest1\v1\priest1_v1_2.png",
+                r"assets\2D Pixel Dungeon Asset Pack\Character_animation\priests_idle\priest1\v1\priest1_v1_3.png",
+                r"assets\2D Pixel Dungeon Asset Pack\Character_animation\priests_idle\priest1\v1\priest1_v1_4.png",
+            ]
+        else:
+            self.frame_paths = [
+                r'assets/2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest1/v1/priest1_v1_1.png',
+                r"assets/2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest1/v1/priest1_v1_2.png",
+                r"assets/2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest1/v1/priest1_v1_3.png",
+                r"assets/2D Pixel Dungeon Asset Pack/Character_animation/priests_idle/priest1/v1/priest1_v1_4.png",
+            ]
+
+        self.frames = [pr.load_texture(path) for path in self.frame_paths]
         self.frame_count = len(self.frames)
 
-        # Animation variables
         self.current_frame = 0
         self.frame_speed = 8
         self.frame_timer = 0
 
-        # Movement variables
-        self.player_pos = [400, 300]  # Initial position
-        self.mov_speed = 4  # Jump speed
-        self.gravity = 0.3  # Gravity effect
-        self.velocity_y = 0  # Vertical velocity
+        self.player_pos = [400, 300]
+        self.mov_speed = 4
+        self.gravity = 0.3
+        self.velocity_y = 0
         self.is_jumping = False
 
     def move(self):
-        if is_key_pressed(KEY_W) or is_key_pressed(KEY_SPACE):
+        if pr.is_key_pressed(rp.KEY_W) or pr.is_key_pressed(rp.KEY_SPACE):
             self.velocity_y = -7
-
 
         self.velocity_y += self.gravity
         self.player_pos[1] += self.velocity_y
+
+        if self.player_pos[1] < 0:
+            self.player_pos[1] = 0
+
+        if self.player_pos[1] >= 638:
+            self.player_pos[1] = 638
+            if pr.is_key_pressed(rp.KEY_W) or pr.is_key_pressed(rp.KEY_SPACE):
+                self.velocity_y = -7
+        
+        self.gravity = 0.3
